e277d617677415cc3f4e271c8f65c1469e635f86
Sophie Aubert <sophie.aubert@heig-vd.ch>
2023-01-31 12:59:54 +0100

    bordel !!!!

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 "use strict";
-
 // On importe la classe Deck de Deck.js (comme demandé)
 import {Deck} from "./Deck.js"
 import {Card} from "./Card.js"
-
 // On exporte cette constante dans Deck.js
 export const options = {
     values: ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"],
     suits: ["Hearts", "Diamonds", "Spades", "Clubs"]
 }
-
 
 // ICI ON CREE : 
 // UN DECK (=monDeck)
@@ -17,13 +14,14 @@ export const options = {
 // - LA METHODE DE CREATION DU DECK
 // - LA METHODE D'AFFICHEAGE
 
-//const monDeck = new Deck(options)
-//monDeck.createFullDeck(),
-//monDeck.shuffle()
-//monDeck.displayCards()
+const monDeck = new Deck(options)
+monDeck.createFullDeck(),
+monDeck.shuffle()
+monDeck.displayCards()
 
 // ON PEUT AUSSI FAIRE 
-const monDeck = new Deck(options).createFullDeck().shuffle().displayCards()
+//const monDeck = new Deck(options).createFullDeck().shuffle().displayCards()
+// mais ATTENTION ! il faut faire des retun this dans chaque méthode...
 
 // console.log(monDeck)
 
